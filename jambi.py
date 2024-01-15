f4288d0838174956799aea04c6eba14d53c0914a
Bruno Lauwers <mail@brunolauwers.nl>
2024-01-15 22:17:06 +0100

    Fix outputting to separate folder, including assets.

--- jambi.py
+++ jambi.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+import os
+import shutil
+
 from config.config import load_config
 from rendering_engine.template_rendering import render_template
 from repository.page_repository import get_pages
@@ -6,9 +9,18 @@ from repository.page_repository import get_pages
 def build(env: str) -> None:
     load_config(env)
     pages = get_pages()
+    output_folder = "output"
+    css_folder = output_folder + "/assets"
+
+    if os.path.exists(output_folder):
+        shutil.rmtree(output_folder, ignore_errors=True)
+
+    os.mkdir(output_folder)
+
     for page in pages:
-        render_template('infinite8.html', page)
+        render_template('infinite8.html', output_folder, page)
 
-
+    shutil.copytree('assets', css_folder, symlinks=False, ignore=None, copy_function=shutil.copy2, ignore_dangling_symlinks=False,
+                    dirs_exist_ok=False)
 if __name__ == "__main__":
     build("dev")
--- rendering_engine/template_rendering.py
+++ rendering_engine/template_rendering.py
@@ -1,9 +1,9 @@
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader
 from markdown import markdown
 
 from repository.model.model import Page
 
-def render_template(template_name: str, page: Page) -> None:
+def render_template(template_name: str, output_folder: str, page: Page) -> None:
     env = Environment(
         loader=FileSystemLoader(searchpath="./templates"),
         autoescape=False
@@ -12,9 +12,9 @@ def render_template(template_name: str, page: Page) -> None:
     content_as_html = markdown(page.content)
     template = env.get_template(template_name)
     output = template.render(page_title=page.title, content=content_as_html)
-    return _output_to_file(file_name=page.file_name, output=output)
+    return _output_to_file(file_name=page.file_name, output=output, output_folder=output_folder)
 
-def _output_to_file(file_name: str, output: str) -> None:
-    output_file = open(file_name + ".html", "w")
+def _output_to_file(file_name: str, output: str, output_folder: str) -> None:
+    output_file = open(output_folder + "/" + file_name + ".html", "w")
     output_file.write(output)
     return output_file.close()
