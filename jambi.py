494ad3906d28088b189b020c9bbfe78d2b9e6d80
Bruno Lauwers <mail@brunolauwers.nl>
2024-01-17 21:40:09 +0100

    Move assets to templating

--- jambi.py
+++ jambi.py
@@ -20,7 +20,7 @@ def build(env: str) -> None:
     for page in pages:
         render_template(page.template_name + '.html', output_folder, page)
 
-    shutil.copytree('assets', css_folder, symlinks=False, ignore=None, copy_function=shutil.copy2, ignore_dangling_symlinks=False,
+    shutil.copytree('templates/assets', css_folder, symlinks=False, ignore=None, copy_function=shutil.copy2, ignore_dangling_symlinks=False,
                     dirs_exist_ok=False)
 if __name__ == "__main__":
     build("dev")
