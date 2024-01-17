6c637399facc8d02e344e88056baabf7456319e8
Bruno Lauwers <mail@brunolauwers.nl>
2024-01-17 21:29:19 +0100

    Fix query to pages table

--- repository/page_repository.py
+++ repository/page_repository.py
@@ -8,7 +8,7 @@ def get_pages() -> List[Page]:
     connection_string = config.current_config.database_connection_string
     con = sqlite3.connect(connection_string)
     cursor = con.cursor()
-    result = cursor.execute("SELECT title, content, file_name from pages").fetchall()
+    result = cursor.execute("SELECT title, content, file_name, template_name from pages").fetchall()
 
     pages = []
     for page in result:
