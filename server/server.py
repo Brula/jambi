318eb8f0078474b96bf67c7cb9d3f1d95abf84c8
Bruno Lauwers <mail@brunolauwers.nl>
2025-06-08 16:06:35 +0200

    Change flask dependency to fast api

--- server/server.py
+++ server/server.py
@@ -1,32 +1,27 @@
-from flask import Flask, send_from_directory, request
+from fastapi import FastAPI
+from typing import Union
 
 from repository.page_repository import create_page
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route("/")
-def index():
-    return send_from_directory(path="index.html", directory="ui")
+@app.get("/")
+async def root():
+    return {"Hello": "Index page"}
 
 @app.get("/new")
-def new_page_get():
-    return send_from_directory(path="create.html", directory="ui")
+async def new_page_get():
+    return {"Hello": "New page"}
 
 @app.get("/edit")
-def edit_page():
-    # TODO: This should prefill the existing page content in the form.
-    # To keep it simple, this should probably be a button on the index page
-    return send_from_directory(path="create.html", directory="ui")
+async def edit_page():
+    return {"Hello": "Edit page"}
 
 @app.post("/new")
-def new_page_post(request):
+async def new_page_post(request):
     title = request.form.get('title')
     content = request.form.get('content')
     template_name = request.form.get('template_name')
     file_name = request.form.get('file_name')
 
     create_page(title=title, content=content, template_name=template_name, file_name=file_name)
-
-    # TODO: - On startup create db if not exists
-    # TODO: - Redirect to index if succeeded, show error when not
-    # TODO: - Create form to make the request
