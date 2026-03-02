627722e78ffa99ba907a15c99d47fea9ea237dff
Bruno Lauwers <mail@brunolauwers.nl>
2026-03-02 08:15:30 +0100

    Add SQLite database configuration and dependencies

--- config/config.exs
+++ config/config.exs
@@ -59,6 +59,11 @@ config :logger, :default_formatter,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Configure the database
+config :jambi_phoenix, JambiPhoenix.Repo,
+  database: Path.expand("../dev_database/jambi.db", __DIR__),
+  pool_size: 10
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
